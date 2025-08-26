11000cc80a5ae6449cb2e895f5d3457d7f5db24c
Joshua Lin <qtzx06@gmail.com>
2025-08-26 01:20:20 -0700

    feat: mobile css snapping

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,15 +9,14 @@ const Contact = () => {
   });
 
   const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
-  const filter = useTransform(scrollYProgress, [0, 0.5, 1], ["blur(0px)", "blur(0px)", "blur(10px)"]);
+  const filter = useTransform(scrollYProgress, [0, 0.3, 0.7, 1], ["blur(0px)", "blur(0px)", "blur(0px)", "blur(10px)"]);
   const y = useTransform(scrollYProgress, [0, 1], ["0%", "-10%"]);
-
   const zIndex = useTransform(scrollYProgress, (value) => (value > 0.1 && value < 0.9 ? 20 : 0));
 
   return (
-    <div id="contact" ref={ref} className="relative h-screen w-screen bg-white">
+    <div id="contact" ref={ref} className="relative min-h-screen md:h-screen w-screen bg-white">
       <motion.div
-        className="w-full h-full flex flex-col items-center justify-center px-[10vw] text-center"
+        className="w-full h-full flex flex-col items-center justify-center px-[10vw] text-center py-24 md:py-0"
         style={{ scale, y, filter, zIndex }}
       >
         <h2 className="text-4xl md:text-5xl font-bold text-primary mb-4 font-serif">
